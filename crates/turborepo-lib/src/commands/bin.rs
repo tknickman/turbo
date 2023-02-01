7c28330e1841e9f9fe368a374023a0dadc8656d5
Thomas Knickman <tknickman@gmail.com>
2023-02-01 16:07:54 -0500

    Update bin.rs

--- crates/turborepo-lib/src/commands/bin.rs
+++ crates/turborepo-lib/src/commands/bin.rs
@@ -1,6 +1,7 @@
 use std::env::current_exe;
 
 use anyhow::{anyhow, Result};
+// a comment
 
 pub fn run() -> Result<()> {
     let path = current_exe().map_err(|e| anyhow!("could not get path to turbo binary: {}", e))?;
